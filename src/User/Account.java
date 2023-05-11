be7268e7236bd72bc79093fbb67fb777724f1b21
HanniSalman <124783343+HanniSalman@users.noreply.github.com>
2023-05-11 11:38:41 +0200

    account

--- src/User/Account.java
+++ src/User/Account.java
@@ -1,6 +1,7 @@
 package User;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,9 +9,11 @@ import java.util.Scanner;
 public class Account {
     private String mail;
     private String password;
+    private List<User> users;
     public Account(String mail, String password){
         this.mail = mail;
         this.password = password;
+        this.users = new LinkedList<User>();
     }
 
     public String getMail(){
@@ -19,6 +22,14 @@ public class Account {
     }
     public String getPassword(){
         return password;
+    }
+    public User createUser(String name, int age){
+        User user = new User(name, age);
+        return user;
+    }
+    public void deleteUser(String name){
+        //this.users er en ArrayList af denne Accounts users
+        this.users.remove(name);
     }
 
 
