eddc7b1101458113038627d786fb616b95d7406c
babanoelk <124783309+babanoelk@users.noreply.github.com>
2023-05-16 11:00:13 +0200

    q

--- src/Mealplan/MealPlanMain.java
+++ src/Mealplan/MealPlanMain.java
@@ -17,11 +17,14 @@ public class MealPlanMain {
         //DailyMealPlan dailyMealPlan = new DailyMealPlan("test",recipe1);
 
         //MealPlan mealPlan = new MealPlan(listOfDailyMealPlan);
-        DBRecipe db = new DBRecipe();
+        /*DBRecipe db = new DBRecipe();
         Set<Recipe> recipes = db.readRecipes();
         for(Recipe r : recipes) {
             System.out.println(r);
-        }
+        }*/
 
+        MealPlanHandler mealPlanHandler = new MealPlanHandler();
+
+        mealPlanHandler.createMealPlan();
     }
 }
